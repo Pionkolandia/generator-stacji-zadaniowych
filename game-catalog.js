8471ee5d122fab8df33b174c8e89dfe1d01e002f
Pionkolandia <wiechowscy@gmail.com>
2026-07-22 10:50:44 +0200

    Fix production asset paths

--- game-catalog.js
+++ game-catalog.js
@@ -4,301 +4,301 @@ window.STATION_GAME_CATALOG = [
   {
     "title": "Antywirus Mutacja",
     "url": "https://iuvigames.pl/produkt/antywirus_mutacja/",
-    "image": "/public/assets/games/antywirus_mutacja.png"
+    "image": "/assets/games/antywirus_mutacja.png"
   },
   {
     "title": "Arka Noego",
     "url": "https://iuvigames.pl/produkt/arka-noego/",
-    "image": "/public/assets/games/arka-noego.png"
+    "image": "/assets/games/arka-noego.png"
   },
   {
     "title": "Atomowe Zagadki",
     "url": "https://iuvigames.pl/produkt/kwantowe_zagadki/",
-    "image": "/public/assets/games/kwantowe_zagadki.png"
+    "image": "/assets/games/kwantowe_zagadki.png"
   },
   {
     "title": "Baszty i Maszty",
     "url": "https://iuvigames.pl/produkt/baszty-i-maszty/",
-    "image": "/public/assets/games/baszty-i-maszty.png"
+    "image": "/assets/games/baszty-i-maszty.png"
   },
   {
     "title": "Chess Peace",
     "url": "https://iuvigames.pl/produkt/szach-pat/",
-    "image": "/public/assets/games/szach-pat.png"
+    "image": "/assets/games/szach-pat.png"
   },
   {
     "title": "Dinozaury Tajemnicza Wyspa",
     "url": "https://iuvigames.pl/produkt/dinozaury-zaginiona-wyspa/",
-    "image": "/public/assets/games/dinozaury-zaginiona-wyspa.png"
+    "image": "/assets/games/dinozaury-zaginiona-wyspa.png"
   },
   {
     "title": "Droga do Świątyni. Smocza edycja.",
     "url": "https://iuvigames.pl/produkt/droga-do-swiatyni/",
-    "image": "/public/assets/games/droga-do-swiatyni.png"
+    "image": "/assets/games/droga-do-swiatyni.png"
   },
   {
     "title": "Dropzone – strefa zrzutu",
     "url": "https://iuvigames.pl/produkt/dropzone-strefa-zrzutu/",
-    "image": "/public/assets/games/dropzone-strefa-zrzutu.jpg"
+    "image": "/assets/games/dropzone-strefa-zrzutu.jpg"
   },
   {
     "title": "Dziura w Całym",
     "url": "https://iuvigames.pl/produkt/dziura-w-calym/",
-    "image": "/public/assets/games/dziura-w-calym.png"
+    "image": "/assets/games/dziura-w-calym.png"
   },
   {
     "title": "Gnomki i domki",
     "url": "https://iuvigames.pl/produkt/gnomki-i-domki/",
-    "image": "/public/assets/games/gnomki-i-domki.png"
+    "image": "/assets/games/gnomki-i-domki.png"
   },
   {
     "title": "Goool!",
     "url": "https://iuvigames.pl/produkt/goool/",
-    "image": "/public/assets/games/goool.png"
+    "image": "/assets/games/goool.png"
   },
   {
     "title": "IQ Bubbles",
     "url": "https://iuvigames.pl/produkt/iqbubbles/",
-    "image": "/public/assets/games/iqbubbles.png"
+    "image": "/assets/games/iqbubbles.png"
   },
   {
     "title": "IQ Link",
     "url": "https://iuvigames.pl/produkt/iq-link/",
-    "image": "/public/assets/games/iq-link.png"
+    "image": "/assets/games/iq-link.png"
   },
   {
     "title": "IQ Matrix",
     "url": "https://iuvigames.pl/produkt/iq-matrix/",
-    "image": "/public/assets/games/iq-matrix.png"
+    "image": "/assets/games/iq-matrix.png"
   },
   {
     "title": "IQ Noodles",
     "url": "https://iuvigames.pl/produkt/iq-noodles/",
-    "image": "/public/assets/games/iq-noodles.png"
+    "image": "/assets/games/iq-noodles.png"
   },
   {
     "title": "IQ Perplex",
     "url": "https://iuvigames.pl/produkt/iq-perplex/",
-    "image": "/public/assets/games/iq-perplex.png"
+    "image": "/assets/games/iq-perplex.png"
   },
   {
     "title": "IQ Six Pro",
     "url": "https://iuvigames.pl/produkt/iq-six-pro/",
-    "image": "/public/assets/games/iq-six-pro.png"
+    "image": "/assets/games/iq-six-pro.png"
   },
   {
     "title": "IQ Stars",
     "url": "https://iuvigames.pl/produkt/iq-stars/",
-    "image": "/public/assets/games/iq-stars.png"
+    "image": "/assets/games/iq-stars.png"
   },
   {
     "title": "IQ Stixx",
     "url": "https://iuvigames.pl/produkt/iq-stixx/",
-    "image": "/public/assets/games/iq-stixx.png"
+    "image": "/assets/games/iq-stixx.png"
   },
   {
     "title": "IQ Waves",
     "url": "https://iuvigames.pl/produkt/iq-waves/",
-    "image": "/public/assets/games/iq-waves.png"
+    "image": "/assets/games/iq-waves.png"
   },
   {
     "title": "IQ XOXO",
     "url": "https://iuvigames.pl/produkt/iq-xoxo/",
-    "image": "/public/assets/games/iq-xoxo.png"
+    "image": "/assets/games/iq-xoxo.png"
   },
   {
     "title": "Jaś i Małgosia",
     "url": "https://iuvigames.pl/produkt/jas-i-malgosia/",
-    "image": "/public/assets/games/jas-i-malgosia.png"
+    "image": "/assets/games/jas-i-malgosia.png"
   },
   {
     "title": "Kopalnia Złota",
     "url": "https://iuvigames.pl/produkt/kopalnia-zlota/",
-    "image": "/public/assets/games/kopalnia-zlota.png"
+    "image": "/assets/games/kopalnia-zlota.png"
   },
   {
     "title": "Kości Pamięci",
     "url": "https://iuvigames.pl/produkt/kosci_pamieci/",
-    "image": "/public/assets/games/kosci_pamieci.png"
+    "image": "/assets/games/kosci_pamieci.png"
   },
   {
     "title": "Królewna Śnieżka",
     "url": "https://iuvigames.pl/produkt/krolewna-sniezka/",
-    "image": "/public/assets/games/krolewna-sniezka.png"
+    "image": "/assets/games/krolewna-sniezka.png"
   },
   {
     "title": "Królicza Norka",
     "url": "https://iuvigames.pl/produkt/krolicza-norka/",
-    "image": "/public/assets/games/krolicza-norka.png"
+    "image": "/assets/games/krolicza-norka.png"
   },
   {
     "title": "Kubik",
     "url": "https://iuvigames.pl/produkt/kubik/",
-    "image": "/public/assets/games/kubik.png"
+    "image": "/assets/games/kubik.png"
   },
   {
     "title": "Kulki do Kwadratu",
     "url": "https://iuvigames.pl/produkt/kulki_do_kwadratu/",
-    "image": "/public/assets/games/kulki_do_kwadratu.png"
+    "image": "/assets/games/kulki_do_kwadratu.png"
   },
   {
     "title": "Kwadrylion",
     "url": "https://iuvigames.pl/produkt/kwadrylion/",
-    "image": "/public/assets/games/kwadrylion.png"
+    "image": "/assets/games/kwadrylion.png"
   },
   {
     "title": "Logic Lane",
     "url": "https://iuvigames.pl/produkt/domek-w-domek/",
-    "image": "/public/assets/games/domek-w-domek.png"
+    "image": "/assets/games/domek-w-domek.png"
   },
   {
     "title": "Łowcy Duchów",
     "url": "https://iuvigames.pl/produkt/lowcy-duchow/",
-    "image": "/public/assets/games/lowcy-duchow.png"
+    "image": "/assets/games/lowcy-duchow.png"
   },
   {
     "title": "Magiczny Las",
     "url": "https://iuvigames.pl/produkt/magiczny-las/",
-    "image": "/public/assets/games/magiczny-las.png"
+    "image": "/assets/games/magiczny-las.png"
   },
   {
     "title": "Misie w Lesie",
     "url": "https://iuvigames.pl/produkt/misie-w-lesie/",
-    "image": "/public/assets/games/misie-w-lesie.png"
+    "image": "/assets/games/misie-w-lesie.png"
   },
   {
     "title": "Na Plaży",
     "url": "https://iuvigames.pl/produkt/na-plazy/",
-    "image": "/public/assets/games/na-plazy.png"
+    "image": "/assets/games/na-plazy.png"
   },
   {
     "title": "Owieczki",
     "url": "https://iuvigames.pl/produkt/owieczki/",
-    "image": "/public/assets/games/owieczki.png"
+    "image": "/assets/games/owieczki.png"
   },
   {
     "title": "Parada Pingwinów",
     "url": "https://iuvigames.pl/produkt/parada-pingwinow/",
-    "image": "/public/assets/games/parada-pingwinow.png"
+    "image": "/assets/games/parada-pingwinow.png"
   },
   {
     "title": "Parking Puzzler",
     "url": "https://iuvigames.pl/produkt/parking-puzzler/",
-    "image": "/public/assets/games/parking-puzzler.png"
+    "image": "/assets/games/parking-puzzler.png"
   },
   {
     "title": "Pingwiny na Lodzie",
     "url": "https://iuvigames.pl/produkt/pingwiny-na-lodzie/",
-    "image": "/public/assets/games/pingwiny-na-lodzie.png"
+    "image": "/assets/games/pingwiny-na-lodzie.png"
   },
   {
     "title": "Pingwiny – Zabawa w Basenie",
     "url": "https://iuvigames.pl/produkt/pingwiny-zabawa-w-basenie/",
-    "image": "/public/assets/games/pingwiny-zabawa-w-basenie.png"
+    "image": "/assets/games/pingwiny-zabawa-w-basenie.png"
   },
   {
     "title": "Piraci w Morzu Ognia",
     "url": "https://iuvigames.pl/produkt/piraci-w-morzu-ognia/",
-    "image": "/public/assets/games/piraci-w-morzu-ognia.png"
+    "image": "/assets/games/piraci-w-morzu-ognia.png"
   },
   {
     "title": "Plug & Play Ball",
     "url": "https://iuvigames.pl/produkt/plug-play-ball/",
-    "image": "/public/assets/games/plug-play-ball.png"
+    "image": "/assets/games/plug-play-ball.png"
   },
   {
     "title": "Plug & Play Puzzler",
     "url": "https://iuvigames.pl/produkt/plugplay-puzzler/",
-    "image": "/public/assets/games/plugplay-puzzler.png"
+    "image": "/assets/games/plugplay-puzzler.png"
   },
   {
     "title": "Psiaki na Spacerze",
     "url": "https://iuvigames.pl/produkt/psiaki-na-spacerze/",
-    "image": "/public/assets/games/psiaki-na-spacerze.png"
+    "image": "/assets/games/psiaki-na-spacerze.png"
   },
   {
     "title": "Ptasie Figle",
     "url": "https://iuvigames.pl/produkt/ptasie-figle/",
-    "image": "/public/assets/games/ptasie-figle.jpg"
+    "image": "/assets/games/ptasie-figle.jpg"
   },
   {
     "title": "Rafa Koralowa",
     "url": "https://iuvigames.pl/produkt/rafa-koralowa/",
-    "image": "/public/assets/games/rafa-koralowa.png"
+    "image": "/assets/games/rafa-koralowa.png"
   },
   {
     "title": "Robaczki",
     "url": "https://iuvigames.pl/produkt/robaczki/",
-    "image": "/public/assets/games/robaczki.png"
+    "image": "/assets/games/robaczki.png"
   },
   {
     "title": "Rozgryź to!",
     "url": "https://iuvigames.pl/produkt/rozgryz_to/",
-    "image": "/public/assets/games/rozgryz_to.png"
+    "image": "/assets/games/rozgryz_to.png"
   },
   {
     "title": "Spadające Gwiazdy",
     "url": "https://iuvigames.pl/produkt/spadajace-gwiazdy/",
-    "image": "/public/assets/games/spadajace-gwiazdy.png"
+    "image": "/assets/games/spadajace-gwiazdy.png"
   },
   {
     "title": "Strachy na Lachy",
     "url": "https://iuvigames.pl/produkt/strachy_na_lachy/",
-    "image": "/public/assets/games/strachy_na_lachy.png"
+    "image": "/assets/games/strachy_na_lachy.png"
   },
   {
     "title": "Śpiąca Królewna",
     "url": "https://iuvigames.pl/produkt/spiaca-krolewna/",
-    "image": "/public/assets/games/spiaca-krolewna.png"
+    "image": "/assets/games/spiaca-krolewna.png"
   },
   {
     "title": "Tajemnice Świątyni",
     "url": "https://iuvigames.pl/produkt/tajemnice-swiatyni/",
-    "image": "/public/assets/games/tajemnice-swiatyni.png"
+    "image": "/assets/games/tajemnice-swiatyni.png"
   },
   {
     "title": "Take Off",
     "url": "https://iuvigames.pl/produkt/szef-lotow/",
-    "image": "/public/assets/games/szef-lotow.png"
+    "image": "/assets/games/szef-lotow.png"
   },
   {
     "title": "Trzy Bałwanki",
     "url": "https://iuvigames.pl/produkt/balwanki/",
-    "image": "/public/assets/games/balwanki.png"
+    "image": "/assets/games/balwanki.png"
   },
   {
     "title": "Ucieczka z Atlantydy",
     "url": "https://iuvigames.pl/produkt/ucieczka-z-atlantydy/",
-    "image": "/public/assets/games/ucieczka-z-atlantydy.png"
+    "image": "/assets/games/ucieczka-z-atlantydy.png"
   },
   {
     "title": "Ucieczka z lochu",
     "url": "https://iuvigames.pl/produkt/ucieczka-z-lochu/",
-    "image": "/public/assets/games/ucieczka-z-lochu.png"
+    "image": "/assets/games/ucieczka-z-lochu.png"
   },
   {
     "title": "Wilk i 7 Koźlątek",
     "url": "https://iuvigames.pl/produkt/wilk_siedem_kozlatek/",
-    "image": "/public/assets/games/wilk_siedem_kozlatek.png"
+    "image": "/assets/games/wilk_siedem_kozlatek.png"
   },
   {
     "title": "Zgrane Rybki",
     "url": "https://iuvigames.pl/produkt/zgrane_rybki/",
-    "image": "/public/assets/games/zgrane_rybki.png"
+    "image": "/assets/games/zgrane_rybki.png"
   },
   {
     "title": "ZigZag Puzzler",
     "url": "https://iuvigames.pl/produkt/zigzag-puzzler/",
-    "image": "/public/assets/games/zigzag-puzzler.png"
+    "image": "/assets/games/zigzag-puzzler.png"
   },
   {
     "title": "Zwinne Delfinki",
     "url": "https://iuvigames.pl/produkt/zwinne-delfinki/",
-    "image": "/public/assets/games/zwinne-delfinki.png"
+    "image": "/assets/games/zwinne-delfinki.png"
   },
   {
     "title": "Żółwim Tempem",
     "url": "https://iuvigames.pl/produkt/zolwim_tempem/",
-    "image": "/public/assets/games/zolwim_tempem.png"
+    "image": "/assets/games/zolwim_tempem.png"
   }
 ];
